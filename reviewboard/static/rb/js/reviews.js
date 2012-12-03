emou/reviewboard

commit 28878fc92971aac396600224cd16833c99efc0cf

Fix a bug with linkify js globbing parentheses
This further fixes the bug with regards to linkifying text. Now, URLs with
anchors (open and close parens, with or without text in between them) work
properly. This fix doesn't affect the behaviour observed with url wrapped in
parens.

Testing done:
Self tested on own environment with different types of urls
- blah blah (http://www.foo.com/)
- http://download.oracle.com/javase/1.4.2/docs/api/java/lang/Object.html#clone()
- http://download.oracle.com/javase/1.4.2/docs/api/java/lang/Object.html#equals(java.lang.Object)
- gwegdss http://www.foo.com/

Fixes bug 2002
Reviewed at http://reviews.reviewboard.org/r/3374/

reviewboard/static/rb/js/reviews.js
@@ -418,14 +418,18 @@ function linkifyText(text) {
              * We might catch an entity at the end of the URL. This is hard
              * to avoid, since we can't rely on advanced RegExp techniques
              * in all browsers. So, we'll now search for it and prevent it
-             * from being part of the URL if it exists.
+             * from being part of the URL if it exists. However, a URL with 
+             * an open bracket will not have its close bracket removed. This
+             * was a modification to the original bug fix.
              *
              * See bug 1069.
              */
-            var extra = "";
-            var parts = url.match(/^(.*)(&[a-z]+;|\))$/);
 
-            if (parts != null) {
+            var extra = '',
+                parts = url.match(/^(.*)(&[a-z]+;|\))/),
+                openParen = url.match(/.*\(.*/);
+
+            if (parts != null && openParen == null ) {
                 /* We caught an entity. Set it free. */
                 url = parts[1];
                 extra = parts[2];

commit db84b75fbcc86faad9d2156b27bb2f7ec4536754

Fix stripping of last GET parameter in URLs
28878fc92971aac396600224cd16833c99efc0cf has introduced a regression
in reviewboard URL matching. Now links like:
http://foo.com/?param1=foo&param2=bar
will be stripped to
http://foo.com/?param1=foo

reviewboard/static/rb/js/reviews.js
@@ -426,7 +426,7 @@ function linkifyText(text) {
              */
 
             var extra = '',
-                parts = url.match(/^(.*)(&[a-z]+;|\))/),
+                parts = url.match(/^(.*)(&[a-z]+;|\))$/),
                 openParen = url.match(/.*\(.*/);
 
             if (parts != null && openParen == null ) {